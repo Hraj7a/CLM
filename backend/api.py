deff8bc55e5e69686ea2bfa731ab1e9329ba53cb
Hraj7a <huthaifa321@gmail.com>
2026-06-16 12:41:18 +0300

    Fix CORS for frontend

--- backend/api.py
+++ backend/api.py
@@ -42,9 +42,10 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
+        "https://clausecheck-sigma.vercel.app",
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://clausecheck-sigma.vercel.app/",
+        "http://localhost:3000",
     ],
     allow_credentials=True,
     allow_methods=["*"],
